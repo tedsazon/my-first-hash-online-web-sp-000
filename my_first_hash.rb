b2f5c6e6c797fd337c816b8d6b60df571b4067ca
Ted Sazon <tedsazon@gmail.com>
2018-12-19 18:50:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,7 +36,7 @@ def adding
   shipping_manifest["muskets"] = 2# add 2 muskets to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
 
-
+  shipping_manifest
   # add 4 gun powder to the shipping_manifest hash below
 
 
